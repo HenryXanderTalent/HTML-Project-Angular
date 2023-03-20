1d77d3c69479587ee673af0663a555ea03bca8b7
HenryXanderTalent <henry.opara@xandertalent.com>
2023-03-20 12:55:21 +0000

    added about and contact comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
